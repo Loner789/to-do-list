e56c574fb7b577a06b7ec38c1017ca93d418309a
Dmitry <royalmailforloner@gmail.com>
2022-06-17 16:24:26 +0300

    feat: add button-toggle

--- scripts/index.js
+++ scripts/index.js
@@ -1,78 +1,120 @@
+// ПЕРЕМЕННЫЕ:
 const todos = [
-  'Сделать проектную работу',
-  'Полить цветы',
-  'Пройти туториал по Реакту',
-  'Сделать фронт для своего проекта',
-  'Погулять с собакой',
-  'Разобраться в замыканиях',
-  'Решить задачу на Codewars'
-];
+  "Сделать проектную работу",
+  "Полить цветы",
+  "Пройти туториал по Реакту",
+  "Сделать фронт для своего проекта",
+  "Погулять с собакой",
+  "Разобраться в замыканиях",
+  "Решить задачу на Codewars",
+]; // Массив задач "из коробки"
 
-const todoListElement = document.querySelector('.todos__list');
-const todoFormElement = document.querySelector('.todos__form');
-const todoInputElement = todoFormElement.querySelector('.todos__input');
-const todoTemplateElement = document.querySelector('.todo-template');
-const todoSubmitButtonElement = document.querySelector('.todos__submit-btn');
-const getTodoByEvent = e => e.currentTarget.closest('.todo');
-const getTodoTextElement = todo => todo.querySelector('.todo__text');
+const todoListElement = document.querySelector(".todos__list"); // Список задач
+const todoFormElement = document.querySelector(".todos__form"); // Форма добавления задачи
+const todoInputElement = todoFormElement.querySelector(".todos__input"); // Поле ввода
+const todoTemplateElement = document.querySelector(".todo-template"); // Шаблон элемента списка
+const todoSubmitButtonElement = document.querySelector(".todos__submit-btn"); // Кнопка подтверждения
 
 let editedTodo = null;
 
-const deleteTodo = e => {
+// ФУНКЦИИ:
+// Деактивация кнопки
+function disableButton(buttonElement) {
+  buttonElement.disabled = true;
+}
+
+// Активация кнопки
+function enableButton(buttonElement) {
+  buttonElement.disabled = false;
+}
+
+// Переключение кнопки добавления
+function toggleButton(buttonElement) {
+  if (todoInputElement.value.length > 0) {
+    return enableButton(buttonElement);
+  }
+  return disableButton(buttonElement);
+}
+
+// Выбранный элемент списка
+function getTodoByEvent(e) {
+  return e.currentTarget.closest(".todo");
+}
+
+// Текст элемента списка
+function getTodoTextElement(todo) {
+  return todo.querySelector(".todo__text");
+}
+
+// Удаление элемента списка
+function deleteTodo(e) {
   const todo = getTodoByEvent(e);
 
   todo.remove();
-};
+}
 
-const editTodo = e => {
+// Редактирование элемента списка
+function editTodo(e) {
   const todo = getTodoByEvent(e);
 
   editedTodo = todo;
 
   todoInputElement.value = getTodoTextElement(editedTodo).textContent;
-  todoSubmitButtonElement.textContent = 'Сохранить';
-};
+  todoSubmitButtonElement.textContent = "Сохранить";
+  enableButton(todoSubmitButtonElement);
+}
 
-const duplicateTodo = e => {
+// Копирование элемента списка
+function duplicateTodo(e) {
   const todo = getTodoByEvent(e);
   const duplicatedTodo = todo.cloneNode(true);
 
   addTodoListeners(duplicatedTodo);
-  
+
   todo.after(duplicatedTodo);
-};
+}
 
-const addTodoListeners = todo => {
-  todo.querySelector('.todo__btn_type_delete').addEventListener('click', deleteTodo);
-  todo.querySelector('.todo__btn_type_edit').addEventListener('click', editTodo);
-  todo.querySelector('.todo__btn_type_duplicate').addEventListener('click', duplicateTodo);
-};
+// Создание слушателей кнопок элемента списка
+function addTodoListeners(todo) {
+  todo
+    .querySelector(".todo__btn_type_delete")
+    .addEventListener("click", deleteTodo);
+  todo
+    .querySelector(".todo__btn_type_edit")
+    .addEventListener("click", editTodo);
+  todo
+    .querySelector(".todo__btn_type_duplicate")
+    .addEventListener("click", duplicateTodo);
+}
 
-const createTodo = text => {
+// Создание элемента списка
+function createTodo(text) {
   const todo = todoTemplateElement.content
-    .querySelector('.todo')
+    .querySelector(".todo")
     .cloneNode(true);
 
   getTodoTextElement(todo).textContent = text;
   addTodoListeners(todo);
 
   return todo;
-};
+}
 
-const addTodo = text => {
+// Добавление элемента в начало списка
+function addTodo(text) {
   const todo = createTodo(text);
 
   todoListElement.prepend(todo);
-};
+}
 
-const handleTodoSubmit = e => {
+// Поведение кнопки подтверждения
+function handleTodoSubmit(e) {
   e.preventDefault();
 
   const text = todoInputElement.value;
 
   if (editedTodo) {
     getTodoTextElement(editedTodo).textContent = text;
-    todoSubmitButtonElement.textContent = 'Добавить';
+    todoSubmitButtonElement.textContent = "Добавить";
 
     editedTodo = null;
   } else {
@@ -80,8 +122,17 @@ const handleTodoSubmit = e => {
   }
 
   todoFormElement.reset();
-};
+  disableButton(todoSubmitButtonElement);
+}
 
+// Добавление списка "из коробки"
 todos.forEach(addTodo);
 
-todoFormElement.addEventListener('submit', handleTodoSubmit);
+//СЛУШАТЕЛИ СОБЫТИЙ:
+// Слушатель кнопки подтверждения
+todoFormElement.addEventListener("submit", handleTodoSubmit);
+
+// Слушатель поля ввода
+todoInputElement.addEventListener("input", () => {
+  toggleButton(todoSubmitButtonElement);
+});
